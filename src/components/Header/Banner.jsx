5a462bb33677897334157d75f355142fd844f1c1
Sakib928 <sakibgscian@gmail.com>
2024-04-27 19:46:01 +0600

    completed banner section with awesome-reveal and lottie

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -1,16 +1,32 @@
-import { useContext } from "react";
-import { AuthContext } from "../../provider/AuthProvider";
+import Lottie from "lottie-react";
+import animationData from "../../../src/assets/images/artist.json";
+import { Fade, Zoom } from "react-awesome-reveal";
 
 const Banner = () => {
-  const { theme } = useContext(AuthContext);
-  console.log(theme);
-  //   const bannerBg = {theme == light ? 'bg-white' : 'bg-blue-700'}
   return (
-    <div
-      className={`h-[550px] bg-blue-700 ${
-        theme === "light" ? "bg-white" : "bg-blue-700"
-      }`}
-    ></div>
+    <div className="h-[550px] bg-blue-700 flex flex-col md:flex-row justify-between items-center">
+      <div>
+        <Zoom>
+          <h1 className="text-6xl text-white font-extrabold">
+            {" "}
+            Welcome to Scribble
+          </h1>
+        </Zoom>
+        <Fade>
+          <p className="font-medium">
+            {" "}
+            Explore our gallery and let the colors of creativity paint your
+            world with joy
+          </p>
+        </Fade>
+      </div>
+      <div>
+        <Lottie
+          animationData={animationData}
+          className="h-[400px] md:h-[450px] w-[400px] md:w-[450px]"
+        ></Lottie>
+      </div>
+    </div>
   );
 };
 
